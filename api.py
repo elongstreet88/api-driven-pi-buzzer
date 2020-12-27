ace56db2d3a63ee253a5ab13a6d413fe38fed946
elongstreet88 <2525601+elongstreet88@users.noreply.github.com>
2020-12-26 20:57:02 -0600

    Create api.py

---
+++ api.py
@@ -0,0 +1,32 @@
+import flask
+from flask import request, jsonify
+from gpiozero import *
+from time import sleep
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return '''<h1>Distant Reading Archive</h1>
+<p>A prototype API for distant reading of science fiction novels.</p>'''
+
+
+# A route to return all of the available entries in our catalog.
+@app.route('/api/v1/alarm/on', methods=['GET'])
+def api_alarm_on():
+    bz = Buzzer(17)
+    bz.on()
+    sleep(1)
+    return jsonify("on")
+
+@app.route('/api/v1/alarm/off', methods=['GET'])
+def api_alarm_off():
+    bz = Buzzer(17)
+    bz.off()
+    sleep(1)
+    return jsonify("off")
+
+
+app.run()
