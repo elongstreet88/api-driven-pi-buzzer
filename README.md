3260f3a19f2ddac7a658c6fcbfcc45619e6bf755
Eric Longstreet <eric@DFA-MAC-22245.local>
2020-12-30 13:46:59 -0600

    Working prototype with docker.

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # api-driven-pi-buzzer
+
+# Docker Multi Environment Seutp
+```
+docker buildx create --name mybuilder
+docker buildx use mybuilder
+docker buildx inspect --bootstrap
+```
+
+# Docker Build
+docker buildx build --platform linux/amd64,linux/arm64,linux/386,linux/arm/v7,linux/arm/v6 -t elongstreet88/api-driven-pi-buzzer --push .
+
+# Docker Run
+```
+docker run --device /dev/gpiomem -p 5000:5000/tcp elongstreet88/api-driven-pi-buzzer^C
+```
--- api.py
+++ api.py
@@ -9,7 +9,14 @@ app.config["DEBUG"] = True
 
 @app.route('/', methods=['GET'])
 def home():
-    return "<h1>Alarm Prototype</h1>"
+    return """
+<h1>Alarm Prototype</h1>
+
+<h2>Ensure buzzer is hooked up to pin 17 of RasberryPI GPIO<h2>
+
+<h3>To turn on: <a href="/api/v1/alarm/on">/api/v1/alarm/on</a></h3>
+<h3>To turn off: <a href="/api/v1/alarm/off">/api/v1/alarm/off</a></h3>
+    """
 
 
 # A route to return all of the available entries in our catalog.
@@ -27,4 +34,4 @@ def api_alarm_off():
     sleep(1)
     return jsonify("off")
 
-app.run()
+app.run(host='0.0.0.0')
